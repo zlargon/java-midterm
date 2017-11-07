f8313a5db2e6b5e86f372b52134ca888bd27b90d
zlargon <zlargon1988@gmail.com>
2017-11-07 00:21:50 -0500

    40 POINTS: create a transformIntData() method

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -8,9 +8,23 @@ public class MidTerm {
         }
     }
 
+    // add 1 to EACH data element and then multiply the resulting sum by 2 AND and show all data;
+    static public void transformIntData () {
+
+        for (int n : data) {
+            int result = (n + 1) * 2;
+            System.out.print(result + " ");
+        }
+    }
+
     static void demo () {
         // 1. create a showIntData() method.
         System.out.println("showIntData():");
         MidTerm.showIntData();
+
+        // 2. create a transformIntData() method:
+        // add 1 to EACH data element and then multiply the resulting sum by 2 AND and show all data;
+        System.out.println("\n\ntransformIntData():");
+        MidTerm.transformIntData();
     }
 }
