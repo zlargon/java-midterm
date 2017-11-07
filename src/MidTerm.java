608ca8d91771967d45be5ca6da1a6421dd2fc9c5
zlargon <zlargon1988@gmail.com>
2017-11-07 00:22:57 -0500

    80 POINTS: create a listIteratorTransformIntData1() method

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MidTerm {
 
     static private int[] data = { 1, 2, 3 };
@@ -17,6 +21,21 @@ public class MidTerm {
         }
     }
 
+    // Use ListIterator to transform (add 1 and multiply sum by 2) the original data and show the transformed data
+    static public void listIteratorTransformIntData1 () {
+        List <Integer> numbers = new ArrayList<Integer>();
+        for (int n : data) {
+            numbers.add(n);
+        }
+
+        Iterator<Integer> it = numbers.listIterator();
+        while (it.hasNext()) {
+            int n = it.next();
+            int result = (n + 1) * 2;
+            System.out.print(result + " ");
+        }
+    }
+
     static void demo () {
         // 1. create a showIntData() method.
         System.out.println("showIntData():");
@@ -26,5 +45,10 @@ public class MidTerm {
         // add 1 to EACH data element and then multiply the resulting sum by 2 AND and show all data;
         System.out.println("\n\ntransformIntData():");
         MidTerm.transformIntData();
+
+        // 3. create a listIteratorTransformIntData1() method:
+        // Use ListIterator to transform (add 1 and multiply sum by 2) the original data and show the transformed data;
+        System.out.println("\n\nlistIteratorTransformIntData1():");
+        MidTerm.listIteratorTransformIntData1();
     }
 }
